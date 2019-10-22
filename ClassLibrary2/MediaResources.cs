cf4a8b8492b0f9b8994c57c73e35c59e9b9a696f
Zoltan Rajnai <zoltan.rajnai@cognex.com>
2019-10-22 10:09:12 +0200

    Initial commit

---
+++ ClassLibrary2/MediaResources.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable InconsistentNaming
+
+using System;
+using System.Windows.Media.Imaging;
+
+namespace ClassLibrary2
+{
+    public static class MediaResourceUris
+    {
+        public const string Autofocus_16 = "pack://application:,,,/ClassLibrary2;component/Resources/Autofocus_16.png";
+    }
+
+    public static class MediaResourceImages
+    {
+        public static readonly BitmapImage Autofocus_16 = new BitmapImage(new Uri(MediaResourceUris.Autofocus_16));
+    }
+}
